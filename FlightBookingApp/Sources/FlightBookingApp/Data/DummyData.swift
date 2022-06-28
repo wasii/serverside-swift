f44c37ece4c4aec9b51bb54a459a8c8bf6150717
abdWasiq <abdul.wasiq@naheed.pk>
2022-06-28 16:35:11 +0500

    dummy HTML page integrated

--- FlightBookingApp/Sources/FlightBookingApp/Data/DummyData.swift
+++ FlightBookingApp/Sources/FlightBookingApp/Data/DummyData.swift
@@ -168,3 +168,29 @@ func getFlightsDetailData() -> [[String: Any]] {
          "fareType": "Flex",
          "cancellation":"Cancellation Available"]]
 }
+
+
+func getIndexData() -> String {
+    return """
+<h1 style="color: #5e9ca0;">Server Side Programming in Swift</h1>
+<h2 style="color: #2e6c80;">Swift can do a lot more than we think</h2>
+<p>All the APIs that are being used by Whoosh app are written in Swift using the framework Perfect. Perfect has done all the heavy lifting, and all we need to do is use it's APIs.&nbsp;</p>
+<p>In regards of NodeJS, Perfect is Express for us. In fact, a lot more. Here's what Perfect can do</p>
+<h2 style="color: #2e6c80;">Some features of Perfect:</h2>
+<ol style="list-style: none; font-size: 14px; line-height: 32px; font-weight: bold;">
+<li style="clear: both;">Creating HTTP Server</li>
+<li style="clear: both;">Routing</li>
+<li style="clear: both;">DB Connections (Mongo, MySQL, Maria, Postgres etc)</li>
+<li style="clear: both;">Web Sockets</li>
+<li style="clear: both;">File Handling</li>
+<li style="clear: both;">Server extensions for messaging queues (Kafka, Mosquitto, Zookeeper etc)</li>
+<li style="clear: both;">Deployment (directly on EC2)</li>
+<li style="clear: both;">... and a lot more</li>
+</ol>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+<h2 style="color: #2e6c80;">Let's give it a try:</h2>
+<p><strong>In this week's video, I'll explain what is Perfect, how to start with it, how to handle the HTTP Request, structuring of the project etc. Stay tuned.</strong></p>
+<p>&nbsp;</p>
+<h2 style="color: #2e6c80; text-align: right;"><span style="color: #000000;">iCode</span></h2>
+"""
+}
--- FlightBookingApp/Sources/FlightBookingApp/Routes/Router.swift
+++ FlightBookingApp/Sources/FlightBookingApp/Routes/Router.swift
@@ -13,6 +13,10 @@ import PerfectHTTPServer
 func setupRouter() -> Routes {
     var routes = Routes()
     
+    routes.add(method: .get, uri: "/") { request, response in
+        response.setBody(string: getIndexData())
+            .completed()
+    }
     routes.add(method: .get, uri: "/flights") { request, response in
         FlightListController().handleFlightListRequest(request: request, response: response)
     }
